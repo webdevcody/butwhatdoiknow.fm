e53b2a66bedf6ab0eff3987fa4e4c08ee2fae693
Cody Seibert <webdevcody@gmail.com>
2025-01-05 20:47:51 -0500

    update rss feed to try and fix validation

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -14,8 +14,9 @@ export async function GET() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
   const rss = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
+<rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
+    <atom:link href="${baseUrl}/feed.xml" rel="self" type="application/rss+xml" />
     <title>${escapeXml(podcastInfo.title)}</title>
     <link>${escapeXml(podcastInfo.websiteUrl)}</link>
     <language>${podcastInfo.language}</language>
@@ -25,7 +26,9 @@ export async function GET() {
       <itunes:name>${escapeXml(podcastInfo.author)}</itunes:name>
       <itunes:email>${escapeXml(podcastInfo.email)}</itunes:email>
     </itunes:owner>
-    <itunes:explicit>${podcastInfo.explicit ? "yes" : "no"}</itunes:explicit>
+    <itunes:explicit>${
+      podcastInfo.explicit ? "true" : "false"
+    }</itunes:explicit>
     <itunes:category text="${escapeXml(podcastInfo.category)}"/>
     <itunes:image href="${baseUrl}${podcastInfo.image}"/>
     <description>${escapeXml(podcastInfo.description)}</description>
@@ -40,7 +43,7 @@ export async function GET() {
       <enclosure url="${baseUrl}${episode.file}" type="audio/mpeg" length="0"/>
       <guid isPermaLink="false">${baseUrl}${episode.file}</guid>
       <itunes:duration>${episode.duration}</itunes:duration>
-      <itunes:explicit>${episode.explicit ? "yes" : "no"}</itunes:explicit>
+      <itunes:explicit>${episode.explicit ? "true" : "false"}</itunes:explicit>
       ${
         episode.image ? `<itunes:image href="${baseUrl}${episode.image}"/>` : ""
       }
